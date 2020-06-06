f952c0061587a44551984e24fefe8e823b1ecf6a
Tobias <tal03.mc@gmail.com>
2020-06-06 23:53:17 +0200

    BiSpil now included V2.0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //KoSpil instance = new KoSpil();
-        BiSpil instance = new BiSpil();
-        long time;
+        KoSpil instance = new KoSpil();
+        //BiSpil instance = new BiSpil();
+
+        double time, runTime;
         int n;
 
         if (args.length < 1) {
@@ -11,12 +12,16 @@ public class Main {
             n = Integer.parseInt(args[0]);
         }
 
+        System.out.println("Starting up!");
+        time = System.currentTimeMillis();
         for (int i = 1; i <= n; i++) {
-            time = System.currentTimeMillis();
+            runTime = System.currentTimeMillis();
             instance.run();
-            System.out.println("Run: " + i + " RunTime: " + ((System.currentTimeMillis() - time)/1000.0) + "s");
+            System.out.println("Run: " + i + " RunTime: " + ((System.currentTimeMillis() - runTime)/1000.0) + "s");
         }
 
         instance.stop();
+        time = ((System.currentTimeMillis() -  time)/1000.0);
+        System.out.println("We're done! Running time: " + time/60.0 + " minutes, and average run was " + time/n*1.0 + "s.");
     }
 }
